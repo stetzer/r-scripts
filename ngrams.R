7e6ea7bfd8faea02a1f91c1c7ddf6be4e434ead3
Mark Stetzer <stetzer@gmail.com>
2013-01-21 13:36:58 -0500

    Adding n-grams function

---
+++ ngrams.R
@@ -0,0 +1,10 @@
+library(tau)
+grams <- function(vec, size) {
+  frm <- data.frame(counts=unclass(textcnt(vec, n=size, method="string")))
+  frm$term <- rownames(frm)
+  return(frm[order(-frm$counts),])
+}
+
+#Example usage:
+#unigrams <- grams(c("what is the capital of china", "what is the biggest city in china"), 1)
+#bigrams <- grams(c("what is the capital of china", "what is the biggest city in china"), 1)
